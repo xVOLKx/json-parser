964e1bb8b8e7b6bd145f03f3d957fe86dd6d0bfe
xVOLKx <121677205+xVOLKx@users.noreply.github.com>
2026-05-17 22:42:30 +0300

    Парсер JSON API (jsonplaceholder)

--- parser.js
+++ parser.js
@@ -1,23 +1,12 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
-const url = 'http://cbr.ru'
+
+const url = 'https://example.com';
 
 axios.get(url)
     .then(response => {
         const html = response.data;
-        const $ = cheerio.load(html);
-        const news = [];
-
-        $('a.card').each((i, element) => {
-            const title = $ (element).find('h3').text().trim();
-            const link = $ (element).attr('href');
-            if (title && link) {
-                news.push({ title, link: new URL(link, url).href });
-            }
-        });
-
-        console.log(`Найдено новостей: ${news.length}`);
-        console.log(news.slice(0, 5));
+        console.log(`Длина HTML: ${html.length}`);
+        console.log('Сайт доступен для парсинга');
     })
     .catch(error => {
         console.error('Ошибка при парсинге:', error.message);
